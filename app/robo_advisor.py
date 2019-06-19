0ad49707556ea3b1aa02760665caa094aa27dbf3
Rg3556 <50929687+Rg3556@users.noreply.github.com>
2019-06-19 16:17:01 -0400

    Get latest close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -4,6 +4,8 @@ import requests
 import json
 
 
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
 #
 # INFO INPUTS
 #
@@ -20,6 +22,8 @@ parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]['3. Last Refreshed']
 
+latest_close = parsed_response['Time Series (Daily)']['2019-06-19']['4. close']#> $ & string
+
 # breakpoint()
 
 
@@ -34,7 +38,7 @@ print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
