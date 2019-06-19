779cc56ae44ab3c0f5c3577ecfedf06ace338dd1
Rg3556 <50929687+Rg3556@users.noreply.github.com>
2019-06-19 16:54:11 -0400

    Get recent low

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -33,11 +33,17 @@ latest_close = tsd[latest_day]['4. close']#> $ & string
 # breakpoint()
 
 high_prices = []
+low_prices = []
 
 for date in dates:
     high_price = tsd[date]['2. high']
     high_prices.append(float(high_price))
+    
+    low_price = tsd[date]['3. low']
+    low_prices.append(float(low_price))
+
 recent_high = max(high_prices)
+recent_low = min(low_prices)
 
 
 
@@ -52,7 +58,7 @@ print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
-print("RECENT LOW: $99,000.00")
+print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
