8006cacbd787be3390e3feadb986532ae8062b19
Rg3556 <50929687+Rg3556@users.noreply.github.com>
2019-06-19 16:49:18 -0400

    Get recent high

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -23,14 +23,21 @@ parsed_response = json.loads(response.text)
 last_refreshed = parsed_response["Meta Data"]['3. Last Refreshed']
 
 
-# breakpoint()
+
 
 tsd = parsed_response['Time Series (Daily)']
 dates = list(tsd.keys()) 
 latest_day = dates[0] #> make dynamic
 latest_close = tsd[latest_day]['4. close']#> $ & string
 
+# breakpoint()
 
+high_prices = []
+
+for date in dates:
+    high_price = tsd[date]['2. high']
+    high_prices.append(float(high_price))
+recent_high = max(high_prices)
 
 
 
@@ -44,7 +51,7 @@ print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print("RECENT HIGH: $101,000.00")
+print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
